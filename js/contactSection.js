2a4ca346b07624764a6f790f43d57fef4d30baa6
Christian Fischer <lanz33@gmail.com>
2024-03-15 16:11:22 +0100

    contactlist done, check pls

--- js/contactSection.js
+++ js/contactSection.js
@@ -3,11 +3,18 @@ let uniqueFirstLetters = new Set();
 function initContactSection() {
     let list = document.getElementById('contactSection');
     list.innerHTML = '';
+    list.innerHTML +=`
+    <div class="contactButtonArea dispflex">
+      <div class="borderContactButton dispflex">
+        <div class="addNewContactButton dispflex">Add new contact  
+        <img src="./assets/img/person_add.png"></div>
+      </div> 
+    </div> 
+    `;
     contacts.sort((a, b) => a['lastName'].localeCompare(b['lastName']));
 
     for (i=0; i < contacts.length; i++){
         let firstLetter = contacts[i]['lastName'].charAt(0);
-
         if (!uniqueFirstLetters.has(firstLetter)) {
           uniqueFirstLetters.add(firstLetter);
           list.innerHTML += `
@@ -15,7 +22,13 @@ function initContactSection() {
           <div class="spaceContactList"></div>`;
         }
         list.innerHTML += `
-        <img src="${contacts[i]['url']}" class="contactPic">
-        <div class="singleNameList">${contacts[i]['firstName']} ${contacts[i]['lastName']}</div>`;
-    }
+        <div class="shortContactInfo" id="info${i}">
+          <img src="${contacts[i]['url']}" class="contactPic">
+          <div class="shortContactLetters">
+            <div class="singleNameList">${contacts[i]['firstName']} ${contacts[i]['lastName']}</div>
+            <div class="listMail">${contacts[i]['email']}</div> 
+          </div>
+        </div>`;
+        
+      }
 }
