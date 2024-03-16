2954fa97929583e81e248f41b3cc8fb6a5523b60
ThomasHaberkorn <haberkorn.th@t-online.de>
2024-03-17 00:45:31 +0100

    login und signup final html+css

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 function init() {
     // setItem();
 }
+
+/**
+ * switch from Log in to Sign up in the index.html
+ */
+
+function loginToSignup() {
+    document.getElementById("loginInnerContent").classList.add("d-none");
+    document.getElementById("signinInnerContent").classList.remove("d-none");
+    document.getElementById("loginSignUp").classList.add("d-none");
+}
+
+/**
+ * switch back to Log in Page from Sign up Page in the index.html
+ */
+
+function signupToLogin() {
+    document.getElementById("loginInnerContent").classList.remove("d-none");
+    document.getElementById("signinInnerContent").classList.add("d-none");
+    document.getElementById("loginSignUp").classList.remove("d-none");
+}
