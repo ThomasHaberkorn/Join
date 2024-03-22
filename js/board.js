7434e27ffe7caac20a2b9ad28203399b754fe0bf
karimrustamov <karimrustamov@hotmail.de>
2024-03-22 17:15:47 +0100

    22.03.2024 17:15

--- js/board.js
+++ js/board.js
@@ -2,15 +2,37 @@ document.addEventListener('DOMContentLoaded', function() {
     var taskBoard = document.getElementById('taskBoard');
     var tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-    tasks.forEach(function(task) {
-        var taskElement = document.createElement('div');
-        taskElement.classList.add('task');
-        taskElement.innerHTML = `
-            <h2>${task.title}</h2>
-            <p>${task.description}</p>
-            <p>Due Date: ${task.taskDate}</p>
-            <p>Category: ${task.category}</p>
+    function createTaskCard(task) {
+        var card = document.createElement('div');
+        card.className = 'task-card';
+
+        var priorityColor = 'blue'; // Setzen Sie hier die Logik für die Prioritätsfarbe
+        if (task.priority === 'Urgent') {
+            priorityColor = 'red';
+        } else if (task.priority === 'Medium') {
+            priorityColor = 'yellow';
+        } else {
+            priorityColor = 'green';
+        }
+
+        card.innerHTML = `
+            <div class="task-card-header ${priorityColor}">
+                <span class="task-card-type">${task.category}</span>
+            </div>
+            <div class="task-card-body">
+                <h3 class="task-card-title">${task.title}</h3>
+                <p class="task-card-description">${task.description}</p>
+            </div>
+            <div class="task-card-footer">
+                <div class="task-card-subtasks">${task.subtasks}</div>
+                <div class="task-card-due-date">${task.taskDate}</div>
+            </div>
         `;
-        taskBoard.appendChild(taskElement);
+        return card;
+    }
+
+    tasks.forEach(function(task) {
+        var taskCard = createTaskCard(task);
+        taskBoard.appendChild(taskCard);
     });
 });
