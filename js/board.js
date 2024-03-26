05cbe206e394b3222930cbe56482fac347e3c187
karimrustamov <karimrustamov@hotmail.de>
2024-03-26 19:42:13 +0100

    26.03.24

--- js/board.js
+++ js/board.js
@@ -17,33 +17,33 @@ document.addEventListener('DOMContentLoaded', function () {
         card.id = task.id || 'task-' + Math.random().toString(36).substr(2, 9); // Nutze vorhandene ID oder generiere eine neue
         card.setAttribute('draggable', true);
         card.dataset.status = task.status;
-
+    
+        // Bestimme die Klasse basierend auf der Kategorie
+        let categoryClass = task.category === 'Technical Task' ? 'category-technical' : 'category-userstory';
+    
+        // Erstelle das Div für die Kategorie und wende die Klasse an
+        let categoryDiv = `<div class="${categoryClass}">${task.category}</div>`;
+    
         // Erstellen eines Strings für alle Subtasks
         let subtasksHtml = '<ul class="task-card-subtasks">';
         task.subtasks.forEach(subtask => {
             subtasksHtml += `<li>${subtask}</li>`;
         });
         subtasksHtml += '</ul>';
-
+    
         // Integrieren aller Task-Details einschließlich Datum, Priorität, Kategorie und Subtasks
         card.innerHTML = `
-            <divclass="task-card-header>${task.category}
-            </div>
-            <div class="task-card-title">${task.title}
-            </div>
-            <div class="task-card-description">${task.description}
-            </div>
-            <div>
-            Subtask ...
-            </div>
-            <div>
-            Applied to ...
-            </div>
+            <div class="task-card-header">${categoryDiv}</div>
+            <div class="task-card-title">${task.title}</div>
+            <div class="task-card-description">${task.description}</div>
+            ${subtasksHtml}
+            <div>Applied to ...</div>
         `;
-
+    
         card.addEventListener('dragstart', handleDragStart);
         return card;
     }
+    
 
 
     taskColumns.forEach(column => {
