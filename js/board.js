7e4e2878efdf0963c5d95d989bb3b24481e56eb2
karimrustamov <karimrustamov@hotmail.de>
2024-03-27 17:59:53 +0100

    27.03.24

--- js/board.js
+++ js/board.js
@@ -1,81 +1,152 @@
+// Warte, bis das gesamte HTML-Dokument vollständig geladen ist, bevor der Code ausgeführt wird
 document.addEventListener('DOMContentLoaded', function () {
+
+    // Erfasse alle Spalten für Aufgaben aus dem HTML-Dokument und speichere sie in der Variable taskColumns
     const taskColumns = document.querySelectorAll('.task-column');
+
+    // Lade die Liste der Aufgaben aus dem Local Storage, falls vorhanden, andernfalls setze tasks auf ein leeres Array
     let tasks = JSON.parse(localStorage.getItem('tasks')) || [];
 
-    // Sortiere die Tasks entsprechend ihres Status und füge sie in die entsprechenden Spalten ein
+    // Eine Funktion, die die Initialen der zugewiesenen Kontakte basierend auf deren IDs zurückgibt
+    function getAssignedContactInitials(assignedContactIds) {
+        // Die Funktion erhält als Parameter ein Array von zugewiesenen Kontakt-IDs
+
+        // Die map() Methode wird auf das Array der zugewiesenen Kontakt-IDs angewendet,
+        // um ein neues Array zu erstellen, das die Initialen der Kontakte enthält
+        return assignedContactIds.map(contactId => {
+            // Für jede Kontakt-ID wird eine Funktion ausgeführt, die die Initialen berechnet
+
+            // Suche den Kontakt in der Kontaktliste anhand seiner ID
+            const contact = contacts.find(contact => contact.userID === contactId);
+            // Die find() Methode wird verwendet, um den Kontakt mit der aktuellen Kontakt-ID zu finden
+
+            // Überprüfe, ob der Kontakt gefunden wurde und einen Namen hat
+            if (contact && contact.name) {
+                // Wenn ein Kontakt mit der angegebenen ID gefunden wurde und einen Namen hat
+
+                // Erstelle die Initialen des Kontakts
+                const initials = contact.name.split(' ')
+                    .filter(namePart => namePart.length > 0) // Entferne leere Teile des Namens
+                    .map(namePart => namePart[0].toUpperCase()) // Wähle den ersten Buchstaben jedes Namens und wandele ihn in Großbuchstaben um
+                    .join(''); // Verbinde die Initialen zu einem einzigen String
+                return initials; // Gib die Initialen des Kontakts zurück
+            } else {
+                return '??'; // Wenn der Kontakt nicht gefunden wurde oder keinen Namen hat, gib '??' zurück
+            }
+        }).join(', '); // Verbinde die Initialen aller gefundenen Kontakte zu einem einzigen String und gib sie zurück
+    }
+
+
+    // Eine Funktion, die eine Karte für eine Aufgabe erstellt, basierend auf den Aufgabeninformationen
+    function createTaskCard(task) {
+        // Erstelle ein neues Artikel-Element für die Aufgabenkarte
+        let card = document.createElement('article');
+        card.className = 'task-card';
+        // Setze die ID der Karte auf die ID der Aufgabe oder eine zufällig generierte ID
+        card.id = task.id || 'task-' + Math.random().toString(36).substr(2, 9);
+        card.setAttribute('draggable', true);
+        card.dataset.status = task.status;
+
+        // Bestimme die CSS-Klasse basierend auf der Kategorie der Aufgabe
+        let categoryClass = task.category === 'Technical Task' ? 'category-technical' : 'category-userstory';
+        let categoryDiv = `<div class="${categoryClass}">${task.category}</div>`;
+
+        // Erstelle HTML für die Subtask-Liste
+        let subtasksHtml = '<ul class="task-card-subtasks">';
+        task.subtasks.forEach(subtask => {
+            subtasksHtml += `<li>${subtask}</li>`;
+        });
+        subtasksHtml += '</ul>';
+
+        // Erfasse die Initialen der zugewiesenen Kontakte
+        const assignedContactInitials = getAssignedContactInitials(task.assignedContacts);
+
+        // Setze den HTML-Inhalt der Aufgabenkarte
+        card.innerHTML = `
+            <div class="task-card-header">${categoryDiv}</div>
+            <div class="task-card-title">${task.title}</div>
+            <div class="task-card-description">${task.description}</div>
+            ${subtasksHtml}
+            <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <div>Applied to: ${assignedContactInitials}</div>
+        `;
+
+        // Füge einen Event Listener für den Drag-and-Drop-Vorgang hinzu
+        card.addEventListener('dragstart', handleDragStart);
+        return card;
+    }
+
+    // Iteriere über die vorhandenen Aufgaben und füge sie den entsprechenden Spalten im Board hinzu
     tasks.forEach(task => {
-        // Überprüfen, ob das Element für den Status existiert, um Fehler zu vermeiden
         if (document.getElementById(task.status)) {
             let taskCard = createTaskCard(task);
             document.getElementById(task.status).appendChild(taskCard);
         }
     });
 
-    function createTaskCard(task) {
-        let card = document.createElement('article');
-        card.className = 'task-card';
-        card.id = task.id || 'task-' + Math.random().toString(36).substr(2, 9); // Nutze vorhandene ID oder generiere eine neue
-        card.setAttribute('draggable', true);
-        card.dataset.status = task.status;
-    
-        // Bestimme die Klasse basierend auf der Kategorie
-        let categoryClass = task.category === 'Technical Task' ? 'category-technical' : 'category-userstory';
-    
-        // Erstelle das Div für die Kategorie und wende die Klasse an
-        let categoryDiv = `<div class="${categoryClass}">${task.category}</div>`;
-    
-        // Erstellen eines Strings für alle Subtasks
-        let subtasksHtml = '<ul class="task-card-subtasks">';
-        task.subtasks.forEach(subtask => {
-            subtasksHtml += `<li>${subtask}</li>`;
+    // Eine Funktion, die die Anzeige der Spalten aktualisiert, um anzuzeigen, ob sie Aufgaben enthalten oder nicht
+    function updateTaskColumns() {
+        document.querySelectorAll('.task-column').forEach(column => {
+            const hasTasks = Array.from(column.children).some(child => child.classList.contains('task-card'));
+            const hasNoTaskMessage = !!column.querySelector('.no-task-message');
+
+            if (!hasTasks && !hasNoTaskMessage) {
+                let noTaskMessage = document.createElement('div');
+                noTaskMessage.className = 'no-task-message';
+                noTaskMessage.textContent = 'No task available';
+                column.appendChild(noTaskMessage);
+            } else if (hasTasks && hasNoTaskMessage) {
+                let noTaskMessage = column.querySelector('.no-task-message');
+                column.removeChild(noTaskMessage);
+            }
         });
-        subtasksHtml += '</ul>';
-    
-        // Integrieren aller Task-Details einschließlich Datum, Priorität, Kategorie und Subtasks
-        card.innerHTML = `
-            <div class="task-card-header">${categoryDiv}</div>
-            <div class="task-card-title">${task.title}</div>
-            <div class="task-card-description">${task.description}</div>
-            ${subtasksHtml}
-            <div>Applied to ...</div>
-        `;
-    
-        card.addEventListener('dragstart', handleDragStart);
-        return card;
     }
-    
 
+    // Aktualisiere die Anzeige der Spalten
+    updateTaskColumns();
 
+    // Füge Event Listener für Drag-and-Drop-Operationen zu den Spalten hinzu
     taskColumns.forEach(column => {
         column.addEventListener('dragover', handleDragOver);
         column.addEventListener('drop', handleDrop);
     });
 
+    // Eine Funktion, die aufgerufen wird, wenn ein Drag-and-Drop-Vorgang gestartet wird
     function handleDragStart(e) {
         e.dataTransfer.setData('text/plain', e.target.id);
     }
 
+    // Eine Funktion, die aufgerufen wird, wenn eine Element über eine Drop-Zone bewegt wird
     function handleDragOver(e) {
-        e.preventDefault(); // Erlaubt das Droppen von Elementen
+        e.preventDefault();
     }
 
+    // Eine Funktion, die aufgerufen wird, wenn ein Element in eine Drop-Zone gezogen wird
     function handleDrop(e) {
         e.preventDefault();
         const id = e.dataTransfer.getData('text');
         const draggableElement = document.getElementById(id);
         const newStatus = e.target.closest('.task-column').id;
-        draggableElement.dataset.status = newStatus; // Aktualisiere den Status des Tasks
+        draggableElement.dataset.status = newStatus;
 
         e.target.closest('.task-column').appendChild(draggableElement);
         updateTaskInLocalStorage(id, newStatus);
+        updateTaskColumns();
     }
 
+    // Eine Funktion, die den Status einer Aufgabe im Local Storage aktualisiert
     function updateTaskInLocalStorage(taskId, newStatus) {
-        // Finde den Task im Array und aktualisiere seinen Status
+        // Finde den Index der Aufgabe in der tasks-Liste basierend auf der taskId
         let taskIndex = tasks.findIndex(task => task.id === taskId);
+        // Falls die Aufgabe gefunden wurde
         if (taskIndex !== -1) {
+            // Aktualisiere den Status der Aufgabe
             tasks[taskIndex].status = newStatus;
-            localStorage.setItem('tasks', JSON.stringify(tasks)); // Speichere die aktualisierte Task-Liste im LocalStorage
+            // Speichere die aktualisierte Liste der Aufgaben im Local Storage
+            localStorage.setItem('tasks', JSON.stringify(tasks));
         }
     }
 });
+
