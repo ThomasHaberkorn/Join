e18d0c6565879a5566eb249be61559589e464b2b
ThomasHaberkorn <126246580+ThomasHaberkorn@users.noreply.github.com>
2024-03-30 22:12:56 +0100

    js + css bugfix2

--- js/w3.js
+++ js/w3.js
@@ -15,3 +15,43 @@ async function includeW3() {
         }
     }
 }
+
+function linkToSummary() {
+    window.location.href = "summary.html";
+}
+
+function linkToAddTask() {
+    window.location.href = "add_task.html";
+    // resetFocus();
+    // document.getElementById("addTasksum").classList.add("bgfocus");
+}
+
+function linkToBoard() {
+    window.location.href = "board.html";
+    // resetFocus();
+    document.getElementById("boardSum").classList.add("bgfocus");
+}
+
+function linkToContact() {
+    window.location.href = "contacts.html";
+    // resetFocus();
+    document.getElementById("contactSum").classList.add("bgfocus");
+}
+
+function logout() {
+    sessionStorage.clear();
+    window.location.href = "index.html"; // oder eine andere Logout-Zielseite
+}
+
+let timeout;
+
+function showMenu() {
+    clearTimeout(timeout);
+    dropdownMenu.classList.remove("d-none");
+}
+
+function hideMenu() {
+    timeout = setTimeout(() => {
+        dropdownMenu.classList.add("d-none");
+    }, 700); // Verzögerung in Millisekunden
+}
