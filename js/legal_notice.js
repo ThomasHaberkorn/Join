12b72b5a0c1f2ad7679868ea3097dd98f0b3b4fa
ThomasHaberkorn <126246580+ThomasHaberkorn@users.noreply.github.com>
2024-04-08 08:58:39 +0200

    privacy Policy + links from index and sidabar

--- js/legal_notice.js
+++ js/legal_notice.js
@@ -1,8 +1,32 @@
 async function initLegalNotice() {
     await includeW3();
     legalNoticeActive();
+    showInitials();
+    checkLoggedUserAlt();
+    adjustSidebarVisibility();
+}
+
+async function initPirivacyPolicy() {
+    await includeW3();
+    pirivacyPolicyActive();
+    showInitials();
+    checkLoggedUserAlt();
+    adjustSidebarVisibility();
 }
 
 function legalNoticeActive() {
     document.getElementById("legalNoticeContainer").classList.add("bgfocus");
+}
+
+function pirivacyPolicyActive() {
+    document.getElementById("privacyPolicyContainer").classList.add("bgfocus");
+}
+
+function checkLoggedUserAlt() {
+    let user = sessionStorage.getItem("userName");
+    if (!user) {
+        sessionStorage.setItem("sidebarShouldHide", "true");
+    } else {
+        sessionStorage.removeItem("sidebarShouldHide");
+    }
 }
